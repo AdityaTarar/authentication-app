264183946f3369bc9835e2c8bc4b100f6fbefc69
AdityaTarar <adityatarar8@gmail.com>
2023-12-04 21:20:40 +0530

    removed exec from middleware

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -1,23 +1,22 @@
 const db = require("../models");
 const User = db.user;
 
-checkDuplicateUsernameOrEmail = (req, res, next) => {
-  User.findOne({
-    email: req.body.email,
-  }).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
+checkDuplicateUsernameOrEmail = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
 
     if (user) {
-      res.status(400).send({ message: "Failed! Email is already in use!" });
-      return;
+      return res.status(400).send({ message: "Failed! Email is already in use!" });
     }
 
     next();
-  });
+  } catch (err) {
+    return res.status(500).send({ message: err.message || "An error occurred while checking for duplicate email." });
+  }
 };
+
 
 
 const verifySignUp = {
